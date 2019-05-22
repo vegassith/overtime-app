8f259186c2ae0917b473cb5d83122361e9e3e74b
Mark Murphy <markdmurphy@ymail.com>
2019-05-22 16:36:13 -0700

    Implemented audit log tab that is shown to admin users

---
+++ app/policies/audit_log_policy.rb
@@ -0,0 +1,14 @@
+class AuditLogPolicy < ApplicationPolicy
+
+    def index?
+        return true if admin?   
+    end
+
+    private
+
+    def admin?
+        admin_types.include?(user.type)
+    end
+
+
+end
