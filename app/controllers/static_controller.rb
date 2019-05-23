d2d4834aac686edb7fecd4fa3ca50a0115a74339
Mark Murphy <markdmurphy@ymail.com>
2019-05-23 13:37:07 -0700

    Brought in live data for admin review page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,8 @@
 class StaticController < ApplicationController
 
     def home
-
+        @audit_logs = AuditLog.pending
+        @pending_approvals = Post.submitted
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,13 @@ AdminUser.create(first_name: "Admin",
 
 puts "Created Admin User"
 50.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user1.id, overtime_request: 5.0)
+    Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user1.id, overtime_request: 5.0)
 end
 
 puts "50 posts have been created for user #{@user1.first_name}"
 
 50.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user2.id, overtime_request: 3.5)
+    Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user2.id, overtime_request: 3.5)
 end
 
 puts "50 posts have been created for user #{@user2.first_name}"
