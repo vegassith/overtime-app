92f4d215710152648b869d8129512c12bc8aa397
Mark Murphy <markdmurphy@ymail.com>
2019-05-26 15:54:28 -0700

    Swapped out overtime_requests for daily hours in posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,21 +2,21 @@ FactoryBot.define do
     factory :post do
         date { Date.today }
         rationale { 'Some text goes here' }
-        overtime_request {2.5 }
+        daily_hours {8.5 }
         user
         
     end
     factory :second_post, class: "Post" do
         date { Date.yesterday }
         rationale { 'This is our rationale' }
-        overtime_request {0.5 }
+        daily_hours {7.5 }
         user
     end
 
     factory :post_from_other_user, class: "Post" do
         date { Date.yesterday }
         rationale { 'This is our rationale for other user' }
-        overtime_request {3.5 }
+        daily_hours {8.9 }
         user { FactoryBot.create(:non_authorized_user)}
     end
 end
